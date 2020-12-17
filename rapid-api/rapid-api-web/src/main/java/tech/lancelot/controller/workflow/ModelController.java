c5998789c6cb7832a256f5c111b3d1ea237ad45e
xuli <xuli1@cofco.com>
2020-12-17 14:56:12 +0800

    modified

--- rapid-api/rapid-api-web/src/main/java/tech/lancelot/controller/workflow/ModelController.java
+++ rapid-api/rapid-api-web/src/main/java/tech/lancelot/controller/workflow/ModelController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import tech.lancelot.annotations.Log;
 import tech.lancelot.annotations.restful.AnonymousGetMapping;
 import tech.lancelot.annotations.restful.AnonymousPostMapping;
+import tech.lancelot.service.workflow.ModelService;
 import tech.lancelot.vo.Result;
 
 import java.io.IOException;
@@ -31,16 +32,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ModelController {
 
-    private final RepositoryService repositoryService;
-
-    private final ObjectMapper objectMapper;
+    private final ModelService modelService;
 
     @Log("工作流|模型列表")
     @ApiOperation("工作流|模型列表")
     @GetMapping
     @AnonymousGetMapping
     public Result list() {
-        List<Model> models = repositoryService.createModelQuery().orderByCreateTime().desc().list();
+        List<Model> models = modelService.list();
         return Result.success(models);
     }
 
@@ -49,37 +48,7 @@ public class ModelController {
     @PostMapping
     @AnonymousPostMapping
     public Result create() throws IOException {
-
-        //初始化一个空模型
-        Model model = repositoryService.newModel();
-
-        //设置一些默认信息
-        String name = "new-process";
-        String description = "";
-        int revision = 1;
-        String key = "rapid";
-
-        ObjectNode modelNode = objectMapper.createObjectNode();
-        modelNode.put(ModelDataJsonConstants.MODEL_NAME, name);
-        modelNode.put(ModelDataJsonConstants.MODEL_DESCRIPTION, description);
-        modelNode.put(ModelDataJsonConstants.MODEL_REVISION, revision);
-
-        model.setName(name);
-        model.setKey(key);
-        model.setMetaInfo(modelNode.toString());
-
-        repositoryService.saveModel(model);
-        String id = model.getId();
-
-        //完善ModelEditorSource
-        ObjectNode editorNode = objectMapper.createObjectNode();
-        editorNode.put("id", "canvas");
-        editorNode.put("resourceId", "canvas");
-        ObjectNode stencilSetNode = objectMapper.createObjectNode();
-        stencilSetNode.put("namespace", "http://b3mn.org/stencilset/bpmn2.0#");
-        editorNode.put("stencilset", stencilSetNode);
-        repositoryService.addModelEditorSource(id, editorNode.toString().getBytes("utf-8"));
-
+        String id = modelService.create();
         return Result.success(id);
     }
 
@@ -88,32 +57,7 @@ public class ModelController {
     @PostMapping("/deploy/{id}")
     @AnonymousPostMapping
     public Result deploy(@PathVariable("id") String id) throws Exception {
-
-        //获取模型
-        Model modelData = repositoryService.getModel(id);
-        byte[] bytes = repositoryService.getModelEditorSource(modelData.getId());
-
-        if (bytes == null) {
-            throw new RuntimeException("模型数据为空，请先设计流程并成功保存，再进行发布。");
-        }
-
-        JsonNode modelNode = new ObjectMapper().readTree(bytes);
-
-        BpmnModel model = new BpmnJsonConverter().convertToBpmnModel(modelNode);
-        if (model.getProcesses().size() == 0) {
-            throw new RuntimeException("据模型不符要求，请至少设计一条主线流程。");
-        }
-        byte[] bpmnBytes = new BpmnXMLConverter().convertToXML(model);
-
-        //发布流程
-        String processName = modelData.getName() + ".bpmn20.xml";
-        Deployment deployment = repositoryService.createDeployment()
-                .name(modelData.getName())
-                .addString(processName, new String(bpmnBytes, "UTF-8"))
-                .deploy();
-        modelData.setDeploymentId(deployment.getId());
-        repositoryService.saveModel(modelData);
-
+        modelService.deploy(id);
         return Result.success();
     }
 }
